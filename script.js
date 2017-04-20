40efa65b0a21930d7917f7158c2483603c774536
JodiePebble <maddox.jodie@gmail.com>
2017-04-20 14:46:44 +0100

    changing buttons

--- script.js
+++ script.js
@@ -25,29 +25,29 @@ for(var i; i <= workouts.length -1; i ++){
 }
 
 
-
-if(workoutsContainer){
-    fetch("data.JSON")
-        .then(response => {
-            return response.json();
-        }).then(workouts => {
-            const workoutsHTML = workouts.map(workout => {
-                return `<div class="mdl-shadow--2dp card">
-                    <button class="card_inner mdl-card__actions mdl-button mdl-button--colored mdl-js-button">
-                        <a href=${workout.href}>
-                            <i class="material-icons card-icon">&#xE853;</i>
-                            <div class='card-info'>
-                                <p class='card-title'>${workout.name}</p>
-                                <p class='card-exercise'>${workout.exercises.length} exercises</p>
-                            </div>
-
-                            <i class="material-icons">&#xE147;</i>
-                        </a>
-                    </button>
-                </div>`;
-            }).join("\n");
-            
-            const workoutsContainer = document.getElementById('workouts-content');
-            workoutsContainer.innerHTML = workoutsHTML;
-        });
-}
+//
+//if(workoutsContainer){
+//    fetch("data.JSON")
+//        .then(response => {
+//            return response.json();
+//        }).then(workouts => {
+//            const workoutsHTML = workouts.map(workout => {
+//                return `<div class="mdl-shadow--2dp card">
+//                    <button class="card_inner mdl-card__actions mdl-button mdl-button--colored mdl-js-button">
+//                        <a href=${workout.href}>
+//                            <i class="material-icons card-icon">&#xE853;</i>
+//                            <div class='card-info'>
+//                                <p class='card-title'>${workout.name}</p>
+//                                <p class='card-exercise'>${workout.exercises.length} exercises</p>
+//                            </div>
+//
+//                            <i class="material-icons">&#xE147;</i>
+//                        </a>
+//                    </button>
+//                </div>`;
+//            }).join("\n");
+//            
+//            const workoutsContainer = document.getElementById('workouts-content');
+//            workoutsContainer.innerHTML = workoutsHTML;
+//        });
+//}
