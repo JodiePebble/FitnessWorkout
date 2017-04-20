f6d623d6884c767b8bc37cdbd938276ca2a00b99
JodiePebble <maddox.jodie@gmail.com>
2017-04-20 15:50:23 +0100

    add scripts file to new view

--- script.js
+++ script.js
@@ -35,37 +35,9 @@ function changeView (){
     localStorage.setItem('id', this.id);
 }
 
-var workoutName = document.getElementById('workout-name');
-
-console.log('workoutName', workoutName)
-
 var id = localStorage.getItem('id');
+var workoutName = document.getElementById('workout-name');
+var workoutNumExercises = document.getElementById('workout-exercises');
 
 workoutName.innerHTML = workouts[id].name;
-
-//
-//if(workoutsContainer){
-//    fetch("data.JSON")
-//        .then(response => {
-//            return response.json();
-//        }).then(workouts => {
-//            const workoutsHTML = workouts.map(workout => {
-//                return `<div class="mdl-shadow--2dp card">
-//                    <button class="card_inner mdl-card__actions mdl-button mdl-button--colored mdl-js-button">
-//                        <a href=${workout.href}>
-//                            <i class="material-icons card-icon">&#xE853;</i>
-//                            <div class='card-info'>
-//                                <p class='card-title'>${workout.name}</p>
-//                                <p class='card-exercise'>${workout.exercises.length} exercises</p>
-//                            </div>
-//
-//                            <i class="material-icons">&#xE147;</i>
-//                        </a>
-//                    </button>
-//                </div>`;
-//            }).join("\n");
-//            
-//            const workoutsContainer = document.getElementById('workouts-content');
-//            workoutsContainer.innerHTML = workoutsHTML;
-//        });
-//}
+workoutNumExercises.innerHTML = workouts[id].exercises.length + 'exercises';
