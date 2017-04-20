4204ee7a9408b7b40e0f712a3086815f5522b229
JodiePebble <maddox.jodie@gmail.com>
2017-04-20 14:45:31 +0100

    changing buttons

--- script.js
+++ script.js
@@ -7,6 +7,25 @@ if ('serviceWorker' in navigator) {
 }
 
 const workoutsContainer = document.getElementById('workouts-content');
+
+
+for(var i; i <= workouts.length -1; i ++){
+    workoutsContainer.innerHTML = "<div class='mdl-shadow--2dp card'>" +
+                    "<button class='card_inner mdl-card__actions mdl-button mdl-button--colored mdl-js-button'>"+
+                        "<a id='"+ i +"' href='pages/workout1.html'>" +
+                            "<i class='material-icons card-icon'>&#xE853;</i>" +
+                            "<div class='card-info'>" +
+                                "<p class='card-title'>"+ workouts[i].name + "</p>" +
+                                "<p class='card-exercise'>6 execises</p>" +
+                            "</div>" +
+                            "<i class='material-icons'>&#xE147;</i>"+
+                        "</a>"+
+                    "</button>"+
+                "</div>"
+}
+
+
+
 if(workoutsContainer){
     fetch("data.JSON")
         .then(response => {
