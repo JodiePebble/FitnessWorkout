0f90fed419cb49e17e43786e8feb78deafab6a0d
JodiePebble <maddox.jodie@gmail.com>
2017-04-20 15:22:41 +0100

    add scripts file to new view

--- script.js
+++ script.js
@@ -6,22 +6,25 @@ if ('serviceWorker' in navigator) {
     });
 }
 
-var workoutsContainer = document.getElementById('workouts-content');
+if(document.getElementById('workouts-content'){
 
-for(var i =0; i <= workouts.length -1; i ++){
-    workoutsContainer.innerHTML += "<div class='mdl-shadow--2dp card'>" +
-                    "<button class='card_inner mdl-card__actions mdl-button mdl-button--colored mdl-js-button'>"+
-                        "<a class='goToWorkout' id='"+ i +"' href='pages/workout1.html'>" +
-                            "<i class='material-icons card-icon'>&#xE853;</i>" +
-                            "<div class='card-info'>" +
-                                "<p class='card-title'>"+ workouts[i].name + "</p>" +
-                                "<p class='card-exercise'>6 execises</p>" +
-                            "</div>" +
-                            "<i class='material-icons'>&#xE147;</i>"+
-                        "</a>"+
-                    "</button>"+
-                "</div>";
-    };
+    var workoutsContainer = document.getElementById('workouts-content');
+
+    for(var i =0; i <= workouts.length -1; i ++){
+        workoutsContainer.innerHTML += "<div class='mdl-shadow--2dp card'>" +
+                        "<button class='card_inner mdl-card__actions mdl-button mdl-button--colored mdl-js-button'>"+
+                            "<a class='goToWorkout' id='"+ i +"' href='pages/workout1.html'>" +
+                                "<i class='material-icons card-icon'>&#xE853;</i>" +
+                                "<div class='card-info'>" +
+                                    "<p class='card-title'>"+ workouts[i].name + "</p>" +
+                                    "<p class='card-exercise'>6 execises</p>" +
+                                "</div>" +
+                                "<i class='material-icons'>&#xE147;</i>"+
+                            "</a>"+
+                        "</button>"+
+                    "</div>";
+        };
+}
 
 var goTo = document.getElementsByClassName('goToWorkout');
 
