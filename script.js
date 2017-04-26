74a1839f5e019ce8f5a28b00aaf2b4f9c15debc6
JodiePebble <maddox.jodie@gmail.com>
2017-04-26 19:10:32 +0100

    Changed style of top bar

--- script.js
+++ script.js
@@ -115,7 +115,7 @@ if(existingList){
         existingList.innerHTML += "<div class='exercise'>" +
                                         "<button on-click='displayOverlay()'>" +
                                             "<p>"+ existing_workouts[i].exerciseName +"</p>" +
-                                            "<button class='mdl-button mdl-button--colored mdl-js-button' onclick='addExisting('hello')'>" +
+                                            "<button class='mdl-button mdl-button--colored mdl-js-button' onclick='addExisting("+i+")'>" +
                                                 "<i class='material-icons'>&#xE147;</i>" +
                                             "</button>"+
                                         "</button>" + 
