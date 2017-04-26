24bd5d56a774c933f84e67389a3f88fc76c7bc53
JodiePebble <maddox.jodie@gmail.com>
2017-04-26 19:45:17 +0100

    Changed style of top bar

--- serviceworker.js
+++ serviceworker.js
@@ -2,7 +2,7 @@ var BASE_PATH = '/FitnessWorkout/';
 var CACHE_NAME = 'gih-cache-v1';
 var CACHED_URLS = [
     BASE_PATH + 'create.html',
-    BASE_PATH + 'homeWokrkout.html',
+    BASE_PATH + 'homeWorkout.html',
     BASE_PATH + 'gymWorkout.html',
     BASE_PATH + 'worko.html',
     BASE_PATH + 'settings.html',
