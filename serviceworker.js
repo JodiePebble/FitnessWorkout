25776a9ae9eb7b05b66aff575813e5512aea58f5
JodiePebble <maddox.jodie@gmail.com>
2017-05-02 18:50:56 +0100

    change link name

--- serviceworker.js
+++ serviceworker.js
@@ -55,3 +55,18 @@ self.addEventListener('fetch', function(event) {
     );
   }
 });
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName.startsWith('gih-cache') && CACHE_NAME !== cacheName) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
