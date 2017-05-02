de813921cf5528f9a8493f5dfbed07bed1e6438c
JodiePebble <maddox.jodie@gmail.com>
2017-05-02 18:40:08 +0100

    change link name

--- serviceworker.js
+++ serviceworker.js
@@ -15,13 +15,13 @@ var CACHED_URLS = [
     BASE_PATH + 'scripts/mdl/package.json';
 ];
 
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll(CACHED_URLS);
-    })
-  );
-});
+//self.addEventListener('install', function(event) {
+//  event.waitUntil(
+//    caches.open(CACHE_NAME).then(function(cache) {
+//      return cache.addAll(CACHED_URLS);
+//    })
+//  );
+//});
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
