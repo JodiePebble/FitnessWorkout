90efd9fe85b42e79e733f51ea9a8908b06e8e06d
JodiePebble <maddox.jodie@gmail.com>
2017-05-02 23:37:19 +0100

    change link name

--- offline-map.js
+++ offline-map.js
@@ -1,4 +1,4 @@
-document.getElementById('map-container').style.display = 'none';
-document.getElementById('offline-map-container').style.display = 'block';
-
-console.log('offline');
+//document.getElementById('map-container').style.display = 'none';
+//document.getElementById('offline-map-container').style.display = 'block';
+//
+//console.log('offline');
