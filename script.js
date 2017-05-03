d954e5e1d738ba574763dd62659088bb48437984
JodiePebble <maddox.jodie@gmail.com>
2017-05-03 20:24:44 +0100

    change link name

--- script.js
+++ script.js
@@ -177,7 +177,7 @@ if(existingList){
 //                                        "<button on-click='displayOverlay()'>" +
                                             "<p>"+ existing_workouts[i].exerciseName +"</p>" +
                                             "<button class='mdl-button mdl-button--colored mdl-js-button' onclick='addExisting("+ i +")'>" +
-                                                "<i class='material-icons'>&#xE147;</i>" +
+                                                " <image class='material-icons' src='appImages/remove_circle_black.png'></image>"+
                                             "</button>"+
 //                                        "</button>" + 
                                     "</div>"
