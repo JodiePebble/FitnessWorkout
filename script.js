fd93630836fe2d9fa6d97a94804e43cd38455a3d
JodiePebble <maddox.jodie@gmail.com>
2017-05-08 18:25:14 +0100

    change link name

--- script.js
+++ script.js
@@ -56,17 +56,17 @@ if(workoutsContainer){
     for(var i =0; i <= workouts.length -1; i ++){
         localGetWorkouts.push(workouts[i]);
         
-        workoutsContainer.innerHTML += "<div class='mdl-shadow--2dp card'>" +
-                                            "<button class='card_inner mdl-card__actions mdl-button mdl-button--colored mdl-js-button'>"+
-                                                "<a class='goToWorkout' id='"+ i +"' href='workout1.html'>" +
-                                                    "<image class='material-icons card-icon' src='appImages/fitness_center_black.png'></image>" +
-                                                    "<div class='card-info'>" +
-                                                        "<p class='card-title'>"+ workouts[i].name + "</p>" +
-                                                        "<p class='card-exercise'>"+ workouts[i].exercises.length +  " exercises</p>" +
-                                                    "</div>" +
-                                                "</a>"+
-                                            "</button>"+
-                                        "</div>";
+//        workoutsContainer.innerHTML += "<div class='mdl-shadow--2dp card'>" +
+//                                            "<button class='card_inner mdl-card__actions mdl-button mdl-button--colored mdl-js-button'>"+
+//                                                "<a class='goToWorkout' id='"+ i +"' href='workout1.html'>" +
+//                                                    "<image class='material-icons card-icon' src='appImages/fitness_center_black.png'></image>" +
+//                                                    "<div class='card-info'>" +
+//                                                        "<p class='card-title'>"+ workouts[i].name + "</p>" +
+//                                                        "<p class='card-exercise'>"+ workouts[i].exercises.length +  " exercises</p>" +
+//                                                    "</div>" +
+//                                                "</a>"+
+//                                            "</button>"+
+//                                        "</div>";
     };
     if(localGetWorkouts){
 
